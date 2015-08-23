9af1c47844e435cbe3baa3f1098ec8a62193479f
dans <dans@localhost.localdomain>
2015-08-23 22:52:52 +0200

    init of broker work fine, broker assign channel, missing logic to assign msg to broker

--- unicorn_services/broker/worker.js
+++ unicorn_services/broker/worker.js
@@ -2,8 +2,6 @@
  Template for a worker process for a 'redis-mthread' service.
  By: Lars van der Schans ( 2015 )
  *************************************************/
-var busmsg = require('unicorn/bus-msg');
-
 module.exports = function(config, redis) {
     return {
         startLogic: function () {
@@ -18,63 +16,44 @@ module.exports = function(config, redis) {
              * The raw message comes in and should be processed.
              * When done processing; the callback message must be called in normal (err, message) format.
              */
-            // Catch redis message
             //Getting all the services that are handler for this broker
-            var msg = JSON.parse(message);
+            var msg = JSON.parse(message); // I trust redis to check the msg format
             var channelName;
-            if (!busmsg.verify(msg))
-                console.log('Wrong Message Format!');
-            else {
-                var client = redis.createClient(config.redis.port, config.redis.server, {});
-                if (msg.ack == 0) {
-                    var publicChannel = redis.createClient(config.redis.port, config.redis.server, {});
+            var client = redis.createClient(config.redis.port, config.redis.server, {});
+            if (msg.ack == 0) {
+                var publicChannel = redis.createClient(config.redis.port, config.redis.server, {});
 
-                    if (msg.serviceID == 'broker') {
-                        // assign a channel but also should listen in broker main channel call 'broker'
-                        console.log('broker message');
+                if (msg.serviceID == 'broker') {
+                    // assign a channel but also should listen in broker main channel call 'broker'
+                    console.log('broker message');
+                    channelName = msg.serviceID + '_' + msg.msgpid;
+                    msg.ack = 1;
+                    msg.respondChannel = channelName; //I respond with the particular channel
+                    client.HSET(msg.serviceID, channelName, 1); //start as active
+                    //response
+                    publicChannel.publish('broker-init', JSON.stringify(msg));
+                }else{
+                    if (msg.respondChannel == ' ') { //check if this a message or if this is asking for a channel
+                        // Assign a channel
+                        console.log('I am broker ' + process.pid);
+                        client.HINCRBY('broker_' + process.pid, msg.serviceID, 1);   //Add 1 to the hash of services
+                        channelName = msg.serviceID + '_' + msg.msgpid; // creating service name
+                        client.HINCRBY((msg.serviceID).toString(), channelName.toString(), 1);   //Add 1 to the hash of services
+                        msg.ack = 1; // change the state of the msg to process
+                        msg.respondChannel = channelName;
+                        publicChannel.publish('broker_' + process.pid, JSON.stringify(msg));
+                    }else{ // the msg is not asking for a channel
+                        //pass the message to a service
+                        console.log('I am broker ' + config.process.name + ' processing message... finally');
+                        console.log(client.hget('broker_' + process.pid, msg.serviceID));
+                        //Pushing the channel to the list of channels of the service
                         channelName = msg.serviceID + '_' + msg.msgpid;
                         msg.ack = 1;
-                        //I respond with the particular channel
                         msg.respondChannel = channelName;
-                        client.HSET(msg.serviceID, channelName, 1); //start as active
-                        //response
-                        publicChannel.publish('broker-init', JSON.stringify(msg));
-                    }else{
-                        //check if this a message or if this is asking for a channel
-                        if (msg.respondChannel == ' ') {
-                            // Assign a channel
-                            console.log('i am broker '+process.pid);
-                            client.HINCRBY('broker_'+process.pid, msg.serviceID, 1);   //Add 1 to the hash of services
-                            //Pushing the channel to the list of channels of the service
-                            channelName = msg.serviceID + '_' + msg.msgpid;
-                            client.HINCRBY((msg.serviceID).toString(), channelName.toString(), 1);   //Add 1 to the hash of services
-                            msg.ack = 1;
-                            msg.respondChannel = channelName;
-                            publicChannel.publish('broker_'+process.pid, JSON.stringify(msg));
-                        }else{
-                            //pass the message to a service
-                            //check services and send the message for the service to work
-                            console.log('i am broker '+process.pid+' processing message... finally');
-
-                            //verify is i have this service assign
-                            console.log(client.hget('broker_'+process.pid, msg.serviceID));
-
-                            //Pushing the channel to the list of channels of the service
-                            channelName = msg.serviceID + '_' + msg.msgpid;
-                            msg.ack = 1;
-                            msg.respondChannel = channelName;
-                            publicChannel.publish('broker_'+process.pid, JSON.stringify(msg));
-                        }
-
-                        //I should add one to the broker that is processing this, maybe this should be here CHECK
-                        //here i should add one to the hash of the broker that is handler me
-                        //response
-                        //response
+                        publicChannel.publish('broker_' + process.pid, JSON.stringify(msg));
                     }
                 }
             }
-
-            console.log('Broker listening on this channel for this service ');
 
             callback(null, message);
         }
