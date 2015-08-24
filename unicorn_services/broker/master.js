331172f1c01299c8047beb491fec86375aae0fca
Lars van der Schans <larsvanderschans@gmail.com>
2015-08-25 01:11:27 +0200

    Moved some code around
    Moved pieces of code to other modules to keep the logic sane

--- unicorn_services/broker/master.js
+++ unicorn_services/broker/master.js
@@ -17,7 +17,6 @@ module.exports = function(config, redis) {
             //setInterval(function(){ // Test that will publish stuff
             //    publishClient.publish(config.redis.channel, 'This is a test message');
             //}, 2000);
-
         },
         workLogic: function (message) {
             /*
